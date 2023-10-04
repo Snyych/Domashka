5240272cf39acbccfdb6ff7adfcb78b14fa303b9
Максим Трутнев <justinfoley99@mail.ru>
2023-10-04 14:23:42 +0300

    Теперь программа здоровается с вами на выбранном вами языке

--- hello.pas
+++ hello.pas
@@ -1,3 +1,16 @@
 ﻿begin
-  Println('Привет, мир!');
+  Println('Languages:');
+  Println('1. Russian');
+  Println('2. English');
+  Println('3. French');
+  Println('4. Japanese');
+  Println('5. German');
+  var lang:=ReadInteger('Choose your language:');
+  case lang of
+    1:Print('Привет!');
+    2:Print('Hello!');
+    3:Print('Bonjour!');
+    4:Print('こんにちは!');
+    5:Print('Hallo!');
+  end;
 end.
